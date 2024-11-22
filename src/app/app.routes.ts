04a530bf791208dbd252653bd7df5ff69814d32c
Damian <damijc23@gmail.com>
2024-11-22 00:40:11 -0500

    commit nuevo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,29 +2,10 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
-    {
-        path: '',
-        loadComponent: () => import('./components/slider/slider.component').then(m => m.SliderComponent)
-    },
-
+    
     {
         path:'card',
         loadComponent: () => import('./components/card/card.component').then(m => m.CardComponent)
-    },
-
-    {
-        path:'input',
-        loadComponent: () => import('./components/input/input.component').then(m => m.InputComponent)
-    },
-
-    {
-        path:'observable',
-        loadComponent: () => import('./components/observable/observable.component').then(m => m.ObservableComponent)
-    },
-
-    {
-        path:'form',
-        loadComponent: () => import('./components/formulario/formulario.component').then(m => m.FormularioComponent)
     },
 
     {
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,26 +1,17 @@
 import { Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { MaterialModule } from '../../material/material.module';
-import { SliderComponent } from '../slider/slider.component';
-import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { ObservableService } from '../../services/observable.service';
+
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MaterialModule,SliderComponent,CommonModule],
+  imports: [MaterialModule,CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent implements OnInit, OnChanges, OnDestroy, DoCheck {
   contador=0;
-  reloj:Observable<string>
-  constructor(
-    private observableService:ObservableService
-  ){
-    this.reloj=this.observableService.time;
-    
-    }
   ngOnChanges(changes: SimpleChanges): void {
     this.contador++;
     console.log('On Changes',this.contador,changes);
@@ -43,7 +34,8 @@ longText = `The Shiba Inu is the smallest of the six original and distinct spitz
 from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
 originally bred for hunting.`;
 
-imgUrl='https://material.angular.io/assets/img/examples/shiba2.jpg';
+  imgUrl ='https://rickandmortyapi.com/api/character/avatar/427.jpeg';
+//imgUrl='https://material.angular.io/assets/img/examples/shiba2.jpg';
 
 onEmit(valor:number){
   this.valorDelHijo=valor;
